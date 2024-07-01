ab269a59cd35e49c46df8d2ba70f6fde2e03fbc8
Nayden <naiden.petrov.31.12.00@gmail.com>
2024-07-01 23:40:12 +0300

    Exercise

--- vite_react_app/src/App.jsx
+++ vite_react_app/src/App.jsx
@@ -1,11 +1,22 @@
-import { Button } from '@mui/material';
+import { Button } from "@mui/material";
 
 function App() {
   return (
-    <div className='App'>
-      <Button sx={{ typography: "h1"}} variant='contained' onClick={() => console.log('op')}> My Button</Button>
-    </div >
+    <div className="App">
+      <Button sx={{ typography: "blueTextClass" }} variant="contained">
+        My Button
+      </Button>
+      <Button
+        sx={{
+          bgcolor: "secondary.main",
+          boxShadow: (theme) => theme.shadows[5],
+        }}
+        variant="contained"
+      >
+        My Button
+      </Button>
+    </div>
   );
 }
 
-export default App
+export default App;
--- vite_react_app/src/main.jsx
+++ vite_react_app/src/main.jsx
@@ -1,15 +1,38 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { createTheme, ThemeProvider } from '@mui/material'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createTheme, ThemeProvider } from "@mui/material";
 
-const theme = createTheme();
+const theme = createTheme({
+  palette: {
+    customColors: {
+      royalBlue: "#4169E1",
+    },
+  },
+  components: {
+    MuiButton: {
+      defaultProps: {
+        typ
+      },
+      styleOverrides: {
+        root: {
+          color: "black",
+        },
+      },
+    },
+  },
+  // typography: {
+  //   blueTextClass: {
+  //     color: "red",
+  //   },
+  // },
+});
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
